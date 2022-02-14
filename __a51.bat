43912445951d9dfe08e19462e8c55248d6cc816b
Braedan <bchan616@student.ubc.ca>
2022-02-14 14:29:40 -0800

    Added Capacitor Meter Code

---
+++ __a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\bchan\ELEC291_Project_1\"
+"C:\Users\bchan\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\bchan\ELEC291_Project_1\cap_meter.asm"
+echo Crosside_Action Set_Hex_File C:\Users\bchan\ELEC291_Project_1\cap_meter.HEX
